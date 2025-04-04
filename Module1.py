47372abe8afa61226e6dbf58fbe1059ad42ea36d
SAITEJA123655 <donga.saiteja2604@gmail.com>
2025-04-04 11:50:33 +0530

    Add files via upload

---
+++ Module1.py
@@ -0,0 +1,15 @@
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+A = x + y
+S = x-y
+M = x * y
+D = x/y
+print("Addition: ", A)
+print("Subtraction: ", S)
+print("Multiplication: ", M)
+print("Division: ", D)
+
+x = input("Enter your first name: ")
+y = input("Enter your last name: ")
+Result = x + " " + y + "!" + " Welcome to the python program"
+print(Result)
